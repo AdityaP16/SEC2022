b2290d4bf4d9d234fe90a43ed3d9ce0681de3560
aaryandeshpande <aaryandeshpande3002@gmail.com>
2022-11-05 10:12:17 -0600

    added image code

--- powerups.py
+++ powerups.py
@@ -1,2 +1,4 @@
 import pygame
-shield_img = pygame.image.load('').covert_alpha()
+
+shield_img = pygame.image.load('shield.png').covert_alpha()
+crown_img = pygame.image.load('crown.png').covert_alpha()
