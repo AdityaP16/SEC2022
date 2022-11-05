9279a36b11908d13874230d70a8007b88862781f
Aryan Karadia <aryan.karadia@ucalgary.ca>
2022-11-05 08:48:03 -0600

    pygame test

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 ### main.py
 
 import pygame
+pygame.init()
+
+def main():
+    screen = pygame.display.set_mode([500, 500])
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        
+
+        pygame.draw.circle(screen, (0, 0,  255), (250, 250), 75)
+    
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
+
