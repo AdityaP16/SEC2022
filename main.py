75af156d9e05a667c3f0c44cb03614356f6ace41
Aryan Karadia <aryan.karadia@ucalgary.ca>
2022-11-05 11:28:50 -0600

    select method

--- main.py
+++ main.py
@@ -92,17 +92,48 @@ class Piece:
 
 class Game:
     def __init__(self, win):
+        self._init()
+        self.win = win
+    
+    def update(self):
+        self.board.draw(self.win)
+        pygame.display.update()
+
+    def _init(self):
         self.selected = None
-        self.board = Board()
+        self.board = Board
         self.turn = RED
         self.valid_moves = {}
+    
+    def reset(self):
+        self._init()
+    
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+        if not result:
+            self.selected = None
+            self.select(row. col)
+        
+        else:
+            piece = self.board.get_piece(row, col)
+            if piece != 0 and piece.color == self.turn:
+                self.selected = piece
+                self.valid_moves = self.board.get_valid_moves(piece)
+                return True
+        
+        return False
+    def _move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row. col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
 
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
@@ -113,8 +144,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass  
         
-        board.draw(WIN)
-        pygame.display.update()
+        game.update()
 
     pygame.quit()
 
